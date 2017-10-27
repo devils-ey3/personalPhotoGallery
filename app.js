43878ee5bcca689aac6c74751893f830126d608c
kirainhere <aamiabid@gmail.com>
2017-10-27 18:41:06 +0600

    Bootstrap style added

--- app.js
+++ app.js
@@ -1,26 +1,79 @@
 var app = require('express')();
 var body_parser = require('body-parser');
-app.set("view engine","ejs");
-app.use(body_parser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(body_parser.urlencoded({
+    extended: true
+}));
 var places;
-places = [
-    {title:'shudnrobon',image:'https://i.imgur.com/WtV25d8.jpg',description:'Null'},
-    {title:'drug',image:'https://i.imgur.com/mWROmGo.jpg',description:'Null'},
-    {title:'cross',image:'https://i.imgur.com/fO3Q5K8.jpg',description:'Null'},
-    {title:'banana',image:'https://i.imgur.com/ErxsQ80.jpg',description:'Null'},
-    {title:'fish',image:'https://images.unsplash.com/photo-1485452499676-62ab02c20e83?dpr=1&auto=format&fit=crop&w=767&h=511&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',description:'Null'}
+places = [{
+        title: 'shudnrobon',
+        image: 'https://i.imgur.com/WtV25d8.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'drug',
+        image: 'https://i.imgur.com/mWROmGo.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'cross',
+        image: 'https://i.imgur.com/fO3Q5K8.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'banana',
+        image: 'https://i.imgur.com/ErxsQ80.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'fish',
+        image: 'https://images.unsplash.com/photo-1485452499676-62ab02c20e83?dpr=1&auto=format&fit=crop&w=767&h=511&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
+        description: 'Null'
+    },
+    {
+        title: 'shudnrobon',
+        image: 'https://i.imgur.com/WtV25d8.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'drug',
+        image: 'https://i.imgur.com/mWROmGo.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'cross',
+        image: 'https://i.imgur.com/fO3Q5K8.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'banana',
+        image: 'https://i.imgur.com/ErxsQ80.jpg',
+        description: 'Null'
+    },
+    {
+        title: 'fish',
+        image: 'https://images.unsplash.com/photo-1485452499676-62ab02c20e83?dpr=1&auto=format&fit=crop&w=767&h=511&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
+        description: 'Null'
+    },
+    
 ];
 
-app.get('/',function(request,response){
-    response.render('home',{places:places});
+app.get('/', function (request, response) {
+    response.render('home', {
+        places: places
+    });
 });
 
-app.post('/photoAdded',function(request,response){
-    places.push({ title: request.body.title, image: request.body.image, description: request.body.description });
+app.post('/photoAdded', function (request, response) {
+    places.push({
+        title: request.body.title,
+        image: request.body.image,
+        description: request.body.description
+    });
     response.redirect('/');
 });
 
-app.get('/addPhoto',function(request,response){
+app.get('/addPhoto', function (request, response) {
     response.render("addPhoto");
 });
 
